009e0e6639a01cf832ca55a881b5f93b11e1b67f
Yoshiharu Hirose <yh1224@gmail.com>
2025-01-02 10:22:45 +0900

    use USB Serial on ESP32S3

--- src/app/App.cpp
+++ src/app/App.cpp
@@ -23,6 +23,7 @@ public:
 void App::setup() {
     auto cfg = m5::M5Unified::config();
     cfg.external_spk = true;
+    cfg.serial_baudrate = 115200;
     M5.begin(cfg);
 
     M5.Display.setTextSize(2);
